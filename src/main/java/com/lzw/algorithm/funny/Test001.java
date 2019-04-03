fee9cf87653df46c77b5e8c59bf940c73afb1b6f
jeevanliang <1102257588@qq.com>
2019-04-03 17:11:08 +0800

    引用类型的形参与实参测试

---
+++ src/main/java/com/lzw/algorithm/funny/Test001.java
@@ -0,0 +1,30 @@
+package com.lzw.algorithm.funny;
+
+//测试一下对于引用对象的形参以及实参操作
+
+
+
+public class Test001 {
+    private static  StringBuilder stringBuilder=new StringBuilder("old one");
+
+
+    public static void method(StringBuilder stringBuilder1, StringBuilder stringBuilder2){
+        stringBuilder1.append("-first");
+        stringBuilder2.append("-second");
+
+
+         stringBuilder1=new StringBuilder("new one");
+         stringBuilder1.append("third");
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(stringBuilder);
+
+        method(stringBuilder,stringBuilder);
+
+        System.out.println(stringBuilder);
+    }
+
+
+}
